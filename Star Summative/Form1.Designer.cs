2e82c91b95605563f4c32c0861b2de470616a5bf
ManuelMuncaster <russmunc061@ed.amdsb.ca>
2016-09-28 13:38:42 -0400

    Add project files.

---
+++ Star Summative/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Star_Summative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.drawOrion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Prestige Elite Std", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(65, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(140, 45);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Orion";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.ForeColor = System.Drawing.Color.White;
+            this.labelName.Location = new System.Drawing.Point(8, 221);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(264, 31);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "By: Manuel Muncaster";
+            // 
+            // drawOrion
+            // 
+            this.drawOrion.Location = new System.Drawing.Point(73, 98);
+            this.drawOrion.Name = "drawOrion";
+            this.drawOrion.Size = new System.Drawing.Size(119, 83);
+            this.drawOrion.TabIndex = 2;
+            this.drawOrion.Text = "Draw!!!";
+            this.drawOrion.UseVisualStyleBackColor = true;
+            this.drawOrion.Click += new System.EventHandler(this.drawOrion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.drawOrion);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Orion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button drawOrion;
+    }
+}
+
---
+++ Star Summative/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace Star_Summative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+         
+            InitializeComponent();
+        }
+
+        private void drawOrion_Click(object sender, EventArgs e)
+        {
+
+            Graphics formGraphics = this.CreateGraphics();
+            Graphics fg = this.CreateGraphics();
+
+            labelTitle.Visible = false;
+            drawOrion.Visible = false;
+            labelName.Visible = false;
+
+            //test
+          
+
+
+
+
+        }
+    }
+}
